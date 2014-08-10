99318eb7a0eee8ef422a7c2d879dec43da90f2ed
wowhy <hongy19890331@outlook.com>
2014-08-10 16:36:49 +0800

    1.创建项目 2. 添加扩展方法辅助类 3. 添加反射辅助类

---
+++ All/HyLibrary/CodeCheck.cs
@@ -0,0 +1,22 @@
+﻿namespace HyLibrary
+{
+    using System.Diagnostics.Contracts;
+
+    public static class CodeCheck
+    {
+        public static void Null<TValue>(TValue value, string userMessage)
+        {
+            Contract.Assert(value == null, userMessage);
+        }
+
+        public static void NotNull<TValue>(TValue value, string userMessage)
+        {
+            Contract.Assert(value != null, userMessage);
+        }
+
+        public static void NotEmpty(string value, string userMessage)
+        {
+            Contract.Assert(!string.IsNullOrWhiteSpace(value), userMessage);
+        }
+    }
+}
---
+++ All/HyLibrary/ExtensionMethod/StringExtension.cs
@@ -0,0 +1,69 @@
+﻿namespace HyLibrary.ExtensionMethod
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class StringExtension
+    {
+        private static readonly char[] codes =
+            "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToArray();
+        private static DateTime unix_timestamp = new DateTime(1970, 1, 1);
+
+        /// <summary>
+        /// 生成指定长度的随机字符串
+        /// 生成字符包括 "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        /// </summary>
+        /// <param name="length">字符串长度</param>
+        /// <returns>随机字符串</returns>
+        public static string RandomCode(int length = 4)
+        {
+            StringBuilder sb = new StringBuilder(length + 1);
+            Random rand = new Random((int)(DateTime.Now - unix_timestamp).TotalSeconds);
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(codes[rand.Next(codes.Length)]);
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 将给定字符串随机打乱顺序
+        /// </summary>
+        /// <param name="input">System.String对象</param>
+        /// <returns>随机顺序的"input"</returns>
+        public static string Random(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            Random rand = new Random((int)(DateTime.Now - unix_timestamp).TotalSeconds);
+            var chars = input.ToArray();
+            int p;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                p = rand.Next(chars.Length);
+                char t = chars[i];
+                chars[i] = chars[p];
+                chars[p] = t;
+            }
+
+            return new string(chars);
+        }
+
+        public static byte[] ToBytes(this string input, Encoding encoding)
+        {
+            if (input == null)
+                return null;
+
+            return encoding.GetBytes(input);
+        }
+
+        public static byte[] ToBytes(this string input)
+        {
+            return ToBytes(input, Encoding.UTF8);
+        }
+    }
+}
