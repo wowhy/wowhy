10eaf990f5b7a0d775f5c5c746ac36ecd34b83bf
wowhy <hongy19890331@outlook.com>
2014-08-29 17:03:08 +0800

    修改结构

---
+++ All/SampleParser/Parser/Operator.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Operator.cs" company="Howonder">
+//     Copyright © Howonder 2015. All rights reserved.
+// </copyright>
+// <summary></summary>
+//-----------------------------------------------------------------------
+namespace SampleParser.Parser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum Operator
+    {
+        Plus = '+',
+        Minus = '-',
+        Multi = '*',
+        Div = '/',
+        Mod = '%',
+    }
+}
