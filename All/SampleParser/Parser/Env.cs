3fda02600be7cb9b3f199f58b75fda944b49ecaf
wowhy <hongy19890331@outlook.com>
2014-09-15 22:13:45 +0800

    env

---
+++ All/SampleParser/Parser/Env.cs
@@ -0,0 +1,33 @@
+﻿namespace SampleParser.Parser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Env
+    {
+        private Dictionary<string, Symbol> table;
+
+        protected Env prev;
+
+        public Env(Env p)
+        {
+            table = new Dictionary<string, Symbol>();
+            prev = p;
+        }
+
+        public Symbol Get(string s) 
+        {
+            for (var e = this; e != null; e = e.prev) 
+            {
+                var found = e.table[s];
+                if (found != null)
+                    return found;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ All/SampleParser/Parser/Symbol.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleParser.Parser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Symbol
+    {
+    }
+}
