e466857e2aec89c3a3bd05a3c65daaa0b5557337
wowhy <hongy19890331@outlook.com>
2014-10-08 23:58:35 +0800

    test

--- All/Program01/Program.cs
+++ All/Program01/Program.cs
@@ -22,12 +22,64 @@
 
     public class ChangeValue
     {
+        public object Origin { get; private set; }
+
+        public object Value { get; private set; }
+
+        public Type ValueType { get; private set; }
+
+        public ChangeValue(object origin, object value, Type valueType)
+        {
+            this.Origin = origin;
+            this.Value = value;
+            this.ValueType = valueType;
+        }
     }
 
-    public class ChangeTracker <T> : IEnumerable<KeyValuePair<string, ChangeValue>>
+    public class ChangeTracker<T> : IEnumerable<KeyValuePair<string, ChangeValue>>
         where T : class
     {
         #region Static
+        public static Type TrackType { get; private set; }
+
+        public static Dictionary<string, PropertyInfo> Caches { get; private set; }
+
+        static ChangeTracker()
+        {
+            var type = typeof(T);
+            var properties = type.GetProperties(
+                BindingFlags.Instance
+                | BindingFlags.Public
+                | BindingFlags.GetProperty
+                | BindingFlags.SetProperty)
+                .Where(k => Support(k.PropertyType));
+
+
+            ChangeTracker<T>.TrackType = type;
+            ChangeTracker<T>.Caches = properties.ToDictionary(k => k.Name);
+        }
+
+        private static bool Support(Type type)
+        {
+            if (type.IsGenericType &&
+                type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                type = type.GetGenericArguments()[0];
+            }
+
+            if (type.IsPrimitive ||
+                type.IsEnum ||
+                type == typeof(string) ||
+                type == typeof(decimal) ||
+                type == typeof(DateTime) ||
+                type == typeof(TimeSpan) ||
+                type == typeof(Guid))
+            {
+                return true;
+            }
+
+            return false;
+        }
         #endregion
 
         public ChangeTracker(T origin, T changed)
@@ -37,6 +89,9 @@
 
             this.Origin = origin;
             this.Changed = changed;
+
+            this.Properties = new List<string>();
+            this.ChangeValues = new Dictionary<string, ChangeValue>();
         }
 
         public ChangeValue this[string property]
@@ -50,7 +105,7 @@
         public T Origin { get; protected set; }
         public T Changed { get; protected set; }
 
-        public int Counts { get; protected set; }
+        public int Counts { get { return this.Properties.Count; } }
         public List<string> Properties { get; protected set; }
 
         protected Dictionary<string, ChangeValue> ChangeValues { get; set; }
@@ -65,9 +120,25 @@
             return this.ChangeValues.GetEnumerator();
         }
 
-        protected virtual void Init()
+        public virtual void TrackValueChanged()
         {
+            if (this.Origin == this.Changed)
+                return;
 
+            foreach (var cache in Caches)
+            {
+                var name = cache.Key;
+                var property = cache.Value;
+
+                var v1 = property.GetValue(Origin);
+                var v2 = property.GetValue(Changed);
+
+                if (!object.Equals(v1, v2))
+                {
+                    this.Properties.Add(name);
+                    this.ChangeValues[name] = new ChangeValue(v1, v2, property.PropertyType);
+                }
+            }
         }
     }
 
@@ -75,6 +146,17 @@
     {
         static void Main(string[] args)
         {
+            A a1 = new A { Id = Guid.Empty, Code = "XX", Name = "测试1", Count = 10, JoinDate = new DateTime(1999, 1, 1) };
+            A a2 = new A { Id = Guid.Empty, Code = "XX", Name = "测试2", Count = 20, JoinDate = new DateTime(2000, 1, 1) };
+
+            ChangeTracker<A> tracker = new ChangeTracker<A>(a1, a2);
+            tracker.TrackValueChanged();
+
+            foreach (var value in tracker)
+            {
+                Console.WriteLine("{0}: {1} ====> {2}",
+                    value.Key, value.Value.Origin, value.Value.Value);
+            }
         }
     }
 }
