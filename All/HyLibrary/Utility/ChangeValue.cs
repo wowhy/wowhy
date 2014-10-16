d0a29fc799d9ee49a95fd7e2c818c265405c75a2
wowhy <hongy19890331@outlook.com>
2014-10-16 16:39:12 +0800

    data

---
+++ All/HyLibrary/Utility/ChangeValue.cs
@@ -0,0 +1,25 @@
+﻿namespace HyLibrary.Utility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ChangeValue
+    {
+        public object Origin { get; private set; }
+
+        public object Value { get; private set; }
+
+        public PropertyInfo PropertyInfo { get; private set; }
+
+        public ChangeValue(object origin, object value, PropertyInfo propertyInfo)
+        {
+            this.Origin = origin;
+            this.Value = value;
+            this.PropertyInfo = propertyInfo;
+        }
+    }
+}
