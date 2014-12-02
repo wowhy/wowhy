40c0968306711ca4cbed297bbcc122365d347c9b
wowhy <hongy19890331@outlook.com>
2014-12-02 12:16:50 +0800

    tt template

--- All/Program01/Program.cs
+++ All/Program01/Program.cs
@@ -2,62 +2,12 @@
 {
     using System;
     using System.Data.Entity;
-
-    public class Product 
-    {
-        public Guid Id { get; set; }
-
-        public string Name { get; set; }
-
-        public DateTime CreatedOn { get; set; }
-    }
-
-    public class ProductCheck : Product
-    {
-        public DateTime CheckedOn { get; set; }
-
-        public string Reason { get; set; }
-
-        public Guid ProductId { get; set; }
-
-        public virtual Product Product { get; set; }
-    }
-
-    public class TestDbContext : DbContext
-    {
-        public TestDbContext(string configuration)
-            : base(configuration)
-        {
-        }
-
-        public DbSet<Product> Products { get; set; }
-
-        public DbSet<ProductCheck> ProductChecks { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Product>()
-                        .ToTable("Products");
-
-            modelBuilder.Entity<ProductCheck>()
-                        .Map(k => k.MapInheritedProperties())
-                        .ToTable("ProductChecks")
-                        .HasRequired(k => k.Product)
-                        .WithMany()
-                        .HasForeignKey(k => k.ProductId);
-
-            base.OnModelCreating(modelBuilder);
-        }
-    }
+    using System.Linq;
 
     public class Program
     {
         static void Main(string[] args)
         {
-            using (var context = new TestDbContext("name=test"))
-            {
-                context.Database.CreateIfNotExists();
-            }
         }
     }
 }
