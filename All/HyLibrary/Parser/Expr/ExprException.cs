b2ee144d72d320c6c602174231f6425a2877be72
wowhy <wowhy@outlook.com>
2014-12-09 19:49:28 +0800

    修改命名空间

---
+++ All/HyLibrary/Parser/Expr/ExprException.cs
@@ -0,0 +1,24 @@
+﻿namespace HyLibrary.Parser.Expr
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExprException : ApplicationException
+    {
+        public ExprException(string message) :
+            base(message)
+        {
+        }
+
+        public ExprException(string message, Exception innerException) :
+            base(message, innerException)
+        {
+        }
+    }
+}
