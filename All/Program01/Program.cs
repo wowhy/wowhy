c8f354574af62819b1832b333c3501a851596ea4
wowhy <hongy19890331@outlook.com>
2014-12-15 09:31:47 +0800

    test

--- All/Program01/Program.cs
+++ All/Program01/Program.cs
@@ -13,59 +13,10 @@
     using System.Threading.Tasks;
     using HyLibrary.Lambda;
 
-
-    public class A
-    {
-        public int Id { get; set; }
-
-        public B BB { get; set; }
-    }
-
-    public class B
-    {
-        public int Id { get; set; }
-
-        public C C { get; set; }
-    }
-
-    public class C
-    {
-        public int Id { get; set; }
-    }
-
-    public class TestA
-    {
-        public int Id { get; set; }
-
-        public int BB_Id { get; set; }
-
-        public int BB_C_Id { get; set; }
-    }
-
-
     public class Program
     {
         static void Main(string[] args)
         {
-            A a = new A
-            {
-                Id = 1,
-                BB = new B
-                {
-                    Id = 2,
-                    C = new C 
-                    {
-                        Id = 3
-                    }
-                }
-            };
-
-            List<A> list = new List<A> { a };
-
-            foreach (var item in ProjectionExpression<A, TestA>.To(list))
-            {
-                Console.WriteLine("Id = {0}, B_Id = {1}, B_C_Id = {2}", item.Id, item.BB_Id, item.BB_C_Id);
-            }
         }
     }
 }
